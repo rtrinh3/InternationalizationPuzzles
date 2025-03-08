27b1f90cc0ced9dd6ab59d7d2967b02cd4f7208c
Raymond Trinh <rtrinh3@gmail.com>
2025-03-08 07:38:10 -0500

    Day 2: time zone conversions

---
+++ InternationalizationPuzzles/Puzzle02.cs
@@ -0,0 +1,23 @@
+﻿namespace InternationalizationPuzzles;
+
+// https://i18n-puzzles.com/puzzle/2/
+// Detecting gravitational waves
+public class Puzzle02(string input) : IPuzzle
+{
+    public string Solve()
+    {
+        Dictionary<DateTimeOffset, int> counts = new();
+        foreach (string line in input.TrimEnd().ReplaceLineEndings("\n").Split('\n'))
+        {
+            var parsed = DateTimeOffset.Parse(line);
+            var normalized = parsed.ToUniversalTime();
+            var count = counts.GetValueOrDefault(normalized, 0) + 1;
+            counts[normalized] = count;
+            if (count == 4)
+            {
+                return normalized.ToString("yyyy-MM-ddTHH:mm:sszzz");
+            }
+        }
+        throw new Exception("No solution found");
+    }
+}
---
+++ InternationalizationPuzzlesTests/Puzzle02Tests.cs
@@ -0,0 +1,23 @@
+﻿namespace InternationalizationPuzzles.Tests;
+
+// https://i18n-puzzles.com/puzzle/2/
+// Detecting gravitational waves
+[TestClass()]
+public class Puzzle02Tests
+{
+    [TestMethod()]
+    public void TestInputTest()
+    {
+        var solver = new Puzzle02(File.ReadAllText("02-test-input.txt"));
+        var answer = solver.Solve();
+        Assert.AreEqual("2019-06-05T12:15:00+00:00", answer);
+    }
+
+    [TestMethod()]
+    public void InputTest()
+    {
+        var solver = new Puzzle02(File.ReadAllText("02-input.txt"));
+        var answer = solver.Solve();
+        Assert.AreEqual("2020-10-25T01:30:00+00:00", answer);
+    }
+}
