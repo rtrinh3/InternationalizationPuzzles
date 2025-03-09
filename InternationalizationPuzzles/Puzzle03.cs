6484408bea71eb80dcd5bb9fa5360f27fc9f8ad2
Raymond Trinh <rtrinh3@gmail.com>
2025-03-09 08:06:28 -0400

    Day 3

---
+++ InternationalizationPuzzles/Puzzle03.cs
@@ -0,0 +1,22 @@
+﻿namespace InternationalizationPuzzles;
+
+// https://i18n-puzzles.com/puzzle/3/
+// Unicode passwords
+public class Puzzle03(string input) : IPuzzle
+{
+    public string Solve()
+    {
+        int answer = input.TrimEnd().ReplaceLineEndings("\n").Split('\n').Count(IsValid);
+        return answer.ToString();
+    }
+
+    private static bool IsValid(string password)
+    {
+        return password.Length >= 4
+            && password.Length <= 12
+            && password.Any(char.IsDigit)
+            && password.Any(char.IsUpper)
+            && password.Any(char.IsLower)
+            && password.Any(c => !char.IsAscii(c));
+    }
+}
---
+++ InternationalizationPuzzlesTests/Puzzle03Tests.cs
@@ -0,0 +1,23 @@
+﻿namespace InternationalizationPuzzles.Tests;
+
+// https://i18n-puzzles.com/puzzle/3/
+// Unicode passwords
+[TestClass()]
+public class Puzzle03Tests
+{
+    [TestMethod()]
+    public void TestInputTest()
+    {
+        var solver = new Puzzle03(File.ReadAllText("03-test-input.txt"));
+        var answer = solver.Solve();
+        Assert.AreEqual("2", answer);
+    }
+
+    [TestMethod()]
+    public void InputTest()
+    {
+        var solver = new Puzzle03(File.ReadAllText("03-input.txt"));
+        var answer = solver.Solve();
+        Assert.AreEqual("509", answer);
+    }
+}
