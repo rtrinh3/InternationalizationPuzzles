3fbb268e5e9af6429715c4705436629361073bb2
Raymond Trinh <rtrinh3@gmail.com>
2025-03-10 18:02:06 -0400

    Day 4

---
+++ InternationalizationPuzzles/Puzzle04.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+namespace InternationalizationPuzzles;
+
+// https://i18n-puzzles.com/puzzle/4/
+// A trip around the world
+public class Puzzle04(string input) : IPuzzle
+{
+    public string Solve()
+    {        
+        var normalized = input.TrimEnd().ReplaceLineEndings("\n");
+        var paragraphs = normalized.Split("\n\n");
+
+        int tripTime = 0;
+        foreach (var trip in paragraphs)
+        {
+            DateTime start = new();
+            DateTime end = new();
+            foreach (var line in trip.Split('\n'))
+            {
+                var match = Regex.Match(line, @"^(\S+):\s+(\S+)\s+(\w.+)$");
+                var location = match.Groups[2].Value;
+                var timezone = TimeZoneInfo.FindSystemTimeZoneById(location);
+                var timestamp = match.Groups[3].Value;
+                var localTime = DateTime.ParseExact(timestamp, @"MMM dd, yyyy, HH:mm", System.Globalization.CultureInfo.InvariantCulture);
+                var utcTime = TimeZoneInfo.ConvertTimeToUtc(localTime, timezone);
+                if (match.Groups[1].Value == "Departure")
+                {
+                    start= utcTime;
+                }
+                else if (match.Groups[1].Value == "Arrival")
+                {
+                    end = utcTime;
+                }
+            }
+            tripTime += (int)(end - start).TotalMinutes;
+        }
+        return tripTime.ToString();
+    }
+}
---
+++ InternationalizationPuzzlesTests/Puzzle04Tests.cs
@@ -0,0 +1,23 @@
+﻿namespace InternationalizationPuzzles.Tests;
+
+// https://i18n-puzzles.com/puzzle/4/
+// A trip around the world
+[TestClass()]
+public class Puzzle04Tests
+{
+    [TestMethod()]
+    public void TestInputTest()
+    {
+        var solver = new Puzzle04(File.ReadAllText("04-test-input.txt"));
+        var answer = solver.Solve();
+        Assert.AreEqual("3143", answer);
+    }
+
+    [TestMethod()]
+    public void InputTest()
+    {
+        var solver = new Puzzle04(File.ReadAllText("04-input.txt"));
+        var answer = solver.Solve();
+        Assert.AreEqual("16451", answer);
+    }
+}
