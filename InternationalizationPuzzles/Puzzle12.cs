aec3feb90bf161ec21fbce2debe3053ee25ac315
Raymond Trinh <rtrinh3@gmail.com>
2025-03-18 10:27:33 -0400

    Puzzle 12

---
+++ InternationalizationPuzzles/Puzzle12.cs
@@ -0,0 +1,97 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace InternationalizationPuzzles;
+
+// https://i18n-puzzles.com/puzzle/12/
+// Puzzle 12: Sorting it out
+public class Puzzle12(string input) : IPuzzle
+{
+    private record PhonebookEntry(string LastName, string FirstName, string PhoneNumber);
+
+    private const CompareOptions SortOptions = CompareOptions.IgnoreCase | CompareOptions.IgnoreNonSpace | CompareOptions.IgnoreSymbols;
+
+    private readonly CultureInfo englishCulture = CultureInfo.GetCultureInfo("en");
+    private readonly CultureInfo swedishCulture = CultureInfo.GetCultureInfo("sv");
+    private readonly CultureInfo dutchCulture = CultureInfo.GetCultureInfo("nl");
+    private readonly PhonebookEntry[] phonebook =
+        input.TrimEnd().ReplaceLineEndings("\n").Split('\n').Select(line =>
+        {
+            var match = Regex.Match(line, @"^(?<LastName>.+), (?<FirstName>.+): (?<PhoneNumber>.+)$");
+            return new PhonebookEntry(
+                match.Groups["LastName"].Value,
+                match.Groups["FirstName"].Value,
+                match.Groups["PhoneNumber"].Value
+            );
+        })
+        .ToArray();
+
+    public string Solve()
+    {
+        Debug.Assert(phonebook.Length % 2 == 1);
+        int middleIndex = phonebook.Length / 2;
+        var englishSort = SortEnglish();
+        long englishMiddle = long.Parse(englishSort[middleIndex].PhoneNumber);
+        var swedishSort = SortSwedish();
+        long swedishMiddle = long.Parse(swedishSort[middleIndex].PhoneNumber);
+        var dutchSort = SortDutch();
+        long dutchMiddle = long.Parse(dutchSort[middleIndex].PhoneNumber);
+        long answer = englishMiddle * swedishMiddle * dutchMiddle;
+        return answer.ToString();
+    }
+
+    private static PhonebookEntry[] SortPhonebook(PhonebookEntry[] entries, Comparison<string> nameComparer)
+    {
+        var copy = entries.ToArray();
+        Array.Sort(copy, (a, b) =>
+        {
+            int result = nameComparer(a.LastName, b.LastName);
+            if (result != 0)
+            {
+                return result;
+            }
+            result = nameComparer(a.FirstName, b.FirstName);
+            if (result != 0)
+            {
+                return result;
+            }
+            return a.PhoneNumber.CompareTo(b.PhoneNumber);
+        });
+        return copy;
+    }
+
+    private PhonebookEntry[] SortEnglish() => SortPhonebook(phonebook, CompareEnglish);
+
+    private int CompareEnglish(string a, string b) => string.Compare(a, b, englishCulture, SortOptions);
+
+    private PhonebookEntry[] SortSwedish() => SortPhonebook(phonebook, CompareSwedish);
+
+    private int CompareSwedish(string a, string b) => string.Compare(a, b, swedishCulture, SortOptions);
+
+    private PhonebookEntry[] SortDutch()
+    {
+        var copy = phonebook.Select(entry =>
+        {
+            int lastNameIndex = 0;
+            for (int i = 0; i < entry.LastName.Length; i++)
+            {
+                if (char.IsUpper(entry.LastName[i]))
+                {
+                    lastNameIndex = i;
+                    break;
+                }
+            }
+            if (lastNameIndex >= entry.LastName.Length)
+            {
+                lastNameIndex = 0;
+            }
+            string newLastName = entry.LastName[lastNameIndex..];
+            string newFirstName = (entry.FirstName + " " + entry.LastName[..lastNameIndex]).Trim();
+            return new PhonebookEntry(newLastName, newFirstName, entry.PhoneNumber);
+        }).ToArray();
+        return SortPhonebook(copy, CompareDutch);
+    }
+
+    private int CompareDutch(string a, string b) => string.Compare(a, b, dutchCulture, SortOptions);
+}
---
+++ InternationalizationPuzzlesTests/Puzzle12Tests.cs
@@ -0,0 +1,23 @@
+﻿namespace InternationalizationPuzzles.Tests;
+
+// https://i18n-puzzles.com/puzzle/12/
+// Puzzle 12: Sorting it out
+[TestClass()]
+public class Puzzle12Tests
+{
+    [TestMethod()]
+    public void TestInputTest()
+    {
+        var solver = new Puzzle12(File.ReadAllText("12-test-input.txt"));
+        var answer = solver.Solve();
+        Assert.AreEqual("1885816494308838", answer);
+    }
+
+    [TestMethod()]
+    public void InputTest()
+    {
+        var solver = new Puzzle12(File.ReadAllText("12-input.txt"));
+        var answer = solver.Solve();
+        Assert.AreEqual("3965840697411880", answer);
+    }
+}
