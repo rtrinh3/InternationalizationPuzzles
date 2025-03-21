b48280b4c2ba8ae3f84c3ac14f14193c571ea9f8
Raymond Trinh <rtrinh3@gmail.com>
2025-03-21 13:10:30 -0400

    Puzzle 15: cleanup

--- InternationalizationPuzzles/Puzzle15.cs
+++ InternationalizationPuzzles/Puzzle15.cs
@@ -1,37 +1,34 @@
 ﻿namespace InternationalizationPuzzles;
 
-public class Puzzle15 : IPuzzle
+public class Puzzle15(string input) : IPuzzle
 {
-    private readonly string input;
-
-    private readonly TimeZoneInfo trollTimeZone;
-    private readonly TimeZoneInfo vostokTimeZone;
-
-    public Puzzle15(string input)
-    {
-        this.input = input;
-        var norwayTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Europe/Oslo");
-        var norwayRule = norwayTimeZone.GetAdjustmentRules().Single(rule => rule.DateStart.Year <= 2022 && rule.DateEnd.Year >= 2022);
-        var trollAdjustmentRules = TimeZoneInfo.AdjustmentRule.CreateAdjustmentRule(norwayRule.DateStart, norwayRule.DateEnd, TimeSpan.FromHours(+2), norwayRule.DaylightTransitionStart, norwayRule.DaylightTransitionEnd);
-        this.trollTimeZone = TimeZoneInfo.CreateCustomTimeZone("Antarctica/Troll", TimeSpan.FromHours(+2), "Antarctica/Troll", "Antarctica/Troll", "Antarctica/Troll", [trollAdjustmentRules]);
-        this.vostokTimeZone = TimeZoneInfo.CreateCustomTimeZone("Antarctica/Vostok", TimeSpan.FromHours(+5), "Antarctica/Vostok", "Antarctica/Vostok");
-    }
-
     private record Office(string Name, TimeZoneInfo TimeZone, DateOnly[] Holidays);
 
+    private static TimeZoneInfo MakeTrollTimeZone()
+    {
+        // https://en.wikipedia.org/wiki/Time_in_Norway
+        var norwayTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Europe/Oslo");
+        var norwayRule = norwayTimeZone.GetAdjustmentRules().Single(rule => rule.DateStart.Year <= 2022 && rule.DateEnd.Year >= 2022);
+        var trollRule = TimeZoneInfo.AdjustmentRule.CreateAdjustmentRule(norwayRule.DateStart, norwayRule.DateEnd, TimeSpan.FromHours(+2), norwayRule.DaylightTransitionStart, norwayRule.DaylightTransitionEnd, norwayRule.BaseUtcOffsetDelta);
+        var trollTimeZone = TimeZoneInfo.CreateCustomTimeZone("Antarctica/Troll", TimeSpan.FromHours(+2), "Antarctica/Troll", "Antarctica/Troll Standard", "Antarctica/Troll Daylight", [trollRule]);
+        return trollTimeZone;
+    }
 
-
-    private Office ParseOffice(string officeInput)
+    private static Office ParseOffice(string officeInput)
     {
         var parts = officeInput.Split('\t');
         var name = parts[0];
-        var timeZone = parts[1] switch
+        if (!TimeZoneInfo.TryFindSystemTimeZoneById(parts[1], out var timeZone))
         {
-            "America/Ciudad_Juarez" => TimeZoneInfo.FindSystemTimeZoneById("America/Denver"),
-            "Antarctica/Troll" => trollTimeZone,
-            "Antarctica/Vostok" => vostokTimeZone,
-            _ => TimeZoneInfo.FindSystemTimeZoneById(parts[1])
-        };
+            Console.WriteLine("Timezone not found: " + parts[1] + "; fallback...");
+            timeZone = parts[1] switch
+            {
+                "America/Ciudad_Juarez" => TimeZoneInfo.FindSystemTimeZoneById("America/Denver"),
+                "Antarctica/Troll" => MakeTrollTimeZone(),
+                "Antarctica/Vostok" => TimeZoneInfo.FindSystemTimeZoneById("Etc/GMT-5"),
+                _ => throw new Exception("Unknown time zone: " + parts[1]),
+            };
+        }
         var holidays = parts[2].Split(';').Select(DateOnly.Parse).ToArray();
         return new Office(name, timeZone, holidays);
     }
@@ -39,7 +36,7 @@ public class Puzzle15 : IPuzzle
     public string Solve()
     {
         // Parse
-        var paragraphs = input.TrimEnd().ReplaceLineEndings("\n").Split("\n\n");
+        string[] paragraphs = input.TrimEnd().ReplaceLineEndings("\n").Split("\n\n");
         Office[] supportOffices = paragraphs[0].Split('\n').Select(ParseOffice).ToArray();
         Office[] clients = paragraphs[1].Split('\n').Select(ParseOffice).ToArray();
 
@@ -47,7 +44,7 @@ public class Puzzle15 : IPuzzle
         DateTime yearEnd = new(2023, 1, 1, 0, 0, 0, DateTimeKind.Utc);
         TimeOnly officeStart = new(8, 30);
         TimeOnly officeEnd = new(17, 0);
-        int[] overTimeMinutesPerClient = clients.Select(client =>
+        int[] overTimeMinutesPerClient = clients.AsParallel().Select(client =>
         {
             int overTimeMinutes = 0;
             for (DateTime dt = yearStart; dt < yearEnd; dt = dt.AddMinutes(1))
