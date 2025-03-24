cc5f83b114451462ab7c1520cb2d203cb74c10cb
Raymond Trinh <rtrinh3@gmail.com>
2025-03-24 08:09:18 -0400

    Puzzle 18: stub

---
+++ InternationalizationPuzzles/Puzzle18.cs
@@ -0,0 +1,11 @@
+﻿namespace InternationalizationPuzzles;
+
+// https://i18n-puzzles.com/puzzle/18/
+// Puzzle 18
+public class Puzzle18(string input): IPuzzle
+{
+    public string Solve()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ InternationalizationPuzzlesTests/Puzzle18Tests.cs
@@ -0,0 +1,24 @@
+﻿namespace InternationalizationPuzzles.Tests;
+
+// https://i18n-puzzles.com/puzzle/18/
+// Puzzle 18
+[TestClass()]
+public class Puzzle18Tests
+{
+    [TestMethod()]
+    public void TestInputTest()
+    {
+        var solver = new Puzzle18(File.ReadAllText("18-test-input.txt"));
+        var answer = solver.Solve();
+        Assert.AreEqual("19282", answer);
+    }
+
+    [TestMethod()]
+    public void InputTest()
+    {
+        var solver = new Puzzle18(File.ReadAllText("18-input.txt"));
+        var answer = solver.Solve();
+        //Assert.AreEqual("", answer);
+        Assert.Inconclusive(answer);
+    }
+}
