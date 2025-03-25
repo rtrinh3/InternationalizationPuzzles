b699f4180fda4796ed46e3227609a20e880699f3
Raymond Trinh <rtrinh3@gmail.com>
2025-03-25 12:56:34 -0400

    Puzzle 19 stub

---
+++ InternationalizationPuzzles/Puzzle19.cs
@@ -0,0 +1,24 @@
+﻿namespace InternationalizationPuzzles;
+
+// https://i18n-puzzles.com/puzzle/19/
+public class Puzzle19(string input) : IPuzzle
+{
+    public string Solve()
+    {
+        var lines = input.Split('\n');
+        HashSet<string> places = new();
+        foreach (var line in lines)
+        {
+            var parts = line.Split(';', StringSplitOptions.TrimEntries);
+            var date = DateTime.Parse(parts[0]);
+            var place = parts[1];
+            places.Add(place);
+        }
+        foreach (var place in places)
+        {
+            var tz = TimeZoneInfo.FindSystemTimeZoneById(place);
+        }
+        // TODO
+        throw new NotImplementedException();
+    }
+}
---
+++ InternationalizationPuzzlesTests/Puzzle19Tests.cs
@@ -0,0 +1,23 @@
+﻿namespace InternationalizationPuzzles.Tests;
+
+// https://i18n-puzzles.com/puzzle/19/
+[TestClass()]
+public class Puzzle19Tests
+{
+    [TestMethod()]
+    public void TestInputTest()
+    {
+        var solver = new Puzzle19(File.ReadAllText("19-test-input.txt"));
+        var answer = solver.Solve();
+        Assert.AreEqual("2024-04-09T17:49:00+00:00", answer);
+    }
+
+    [TestMethod()]
+    public void InputTest()
+    {
+        var solver = new Puzzle19(File.ReadAllText("19-input.txt"));
+        var answer = solver.Solve();
+        //Assert.AreEqual("", answer);
+        Assert.Inconclusive(answer);
+    }
+}
